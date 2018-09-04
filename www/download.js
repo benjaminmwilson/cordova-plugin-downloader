30f4a2ba7de6b0edd3592c796a0607a767ca4580
Thembani Fubesi <tfubesi@live.com>
2018-09-04 14:48:48 +0200

    Update download.js

--- www/download.js
+++ www/download.js
@@ -6,7 +6,8 @@ function Download() {
         folder: "folder",
         unzip: false,
         remove: false,
-        timeout: 0
+        timeout: 0,
+        headers: {}
     };
 }
 
@@ -37,6 +38,10 @@ Download.prototype.Initialize = function(settings) {
 
     if(typeof settings.timeout !== "undefined") {
         this.Settings.timeout = settings.timeout;
+    }
+    
+    if(typeof settings.headers !== "undefined") {
+        this.Settings.headers = settings.headers;
     }
 
     if(typeof settings.success !== "undefined") {
@@ -76,6 +81,7 @@ Download.prototype.Get = function(url) {
 
         var xhr = new XMLHttpRequest();
         xhr.open('GET', url, true);
+        xhr.setRequestHeader('Authorization', that.Settings.headers.Authorization);
         xhr.responseType = 'blob';
         xhr.timeout = that.Settings.timeout;
 
